2d2611c1f67f22974378247688542f0d2ec75e30
Jyotish P <me@jyotishp.ml>
2019-04-12 13:31:17 +0530

    Add email login UI

--- pictionary/web/urls.py
+++ pictionary/web/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 
 from . import views
 
@@ -7,7 +9,8 @@ urlpatterns = [
     path('draw/', views.drawer_view, name='draw'),
     path('guess/', views.guesser_view, name='guess'),
     path('play/', views.play_game, name='play_game'),
-    path('login/', views.login, name='login'),
+    # path('login/', views.login, name='login'),
+    path('login/', auth_views.LoginView.as_view(), {'template_name': 'web/login.html'}, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('api/word', views.word),
     path('api/<uuid:game_id>/finish', views.finish)
